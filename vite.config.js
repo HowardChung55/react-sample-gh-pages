2da4717888569fded34c7b14513b2a7769a817da
HChung <t111ab8008@ntut.org.tw>
2023-07-29 18:43:55 +0800

    Update

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base：編譯的路徑，這邊要區別開發中路徑（npm run dev） or 佈署產品的路徑（npm run build）
+  base: process.env.NODE_ENV === "production" ? "/react-sample-gh-pages/" : "/",
   plugins: [react()],
 })
