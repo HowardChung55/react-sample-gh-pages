34b3c762a036bb9f019b7837e5c12389d6122f34
HChung <t111ab8008@ntut.org.tw>
2023-07-30 23:27:47 +0800

    Not a problem.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-// 總管：
+// Main：
 
-// 目錄 node_modules 的套件：
+// 匯入目錄 node_modules 的套件：
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// 目前專案資料夾的檔案：
+// 匯入目前專案資料夾的檔案：
 import App from './App.jsx'
 import './index.css'
 
